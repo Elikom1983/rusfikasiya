15bc8e2a752ebb1ec02b37e3ad21676cc54e050b
Elikom1983 <elyor.usmonov.83@mail.ru>
2023-11-27 23:05:39 +0500

    yangi qoshqganlarim

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+    $(document).ready(function () {
+        $('.owl-carousel').owlCarousel({
+            items: 2, // Adjust the number of items as needed
+            loop: true,
+            margin: 10,
+            nav: true,
+            dots:false,
+            autoplay:true,
+        });
+    });
+
