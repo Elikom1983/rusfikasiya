2a05d28525b0d5fa43bc5b2918f461576e49ff24
Elikom1983 <elyor.usmonov.83@mail.ru>
2023-11-29 20:14:56 +0500

    tovarni tigatdim

--- js/main.js
+++ js/main.js
@@ -1,12 +1,50 @@
 
     $(document).ready(function () {
         $('.owl-carousel').owlCarousel({
-            items: 2, // Adjust the number of items as needed
+            items: 2,
             loop: true,
             margin: 10,
             nav: true,
-            dots:false,
-            autoplay:true,
+            dots: false,
+            autoplay: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 1
+                },
+                1000: {
+                    items: 1
+                }
+                // Добавьте другие размеры экрана и количество отображаемых элементов по вашему выбору
+            }
         });
     });
+    
+    <script>
+    $(document).ready(function () {
+        // Инициализация большого слайдера
+        $('.main-carousel').owlCarousel({
+            items: 1,
+            loop: true,
+            margin: 10,
+            nav: false,
+            dots: false,
+            URLhashListener: true,
+            autoplayHoverPause: true,
+            startPosition: 'URLHash'
+        });
 
+        // Инициализация миниатюрного слайдера
+        $('.thumbnail-carousel').owlCarousel({
+            items: 4,
+            loop: false,
+            margin: 10,
+            nav: true,
+            dots: false,
+            URLhashListener: true,
+            startPosition: 'URLHash'
+        });
+    });
+</script>
