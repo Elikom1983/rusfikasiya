f6b516021806e6d9d244217b911a9f0299cd3315
Elikom1983 <elyor.usmonov.83@mail.ru>
2023-11-29 22:06:21 +0500

    sena qoshildi

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@
             margin: 10,
             nav: true,
             dots: false,
-            autoplay: true,
+            
             responsive: {
                 0: {
                     items: 1
@@ -21,30 +21,56 @@
             }
         });
     });
-    
-    <script>
-    $(document).ready(function () {
-        // Инициализация большого слайдера
-        $('.main-carousel').owlCarousel({
-            items: 1,
-            loop: true,
-            margin: 10,
-            nav: false,
-            dots: false,
-            URLhashListener: true,
-            autoplayHoverPause: true,
-            startPosition: 'URLHash'
-        });
-
-        // Инициализация миниатюрного слайдера
-        $('.thumbnail-carousel').owlCarousel({
-            items: 4,
-            loop: false,
-            margin: 10,
-            nav: true,
-            dots: false,
-            URLhashListener: true,
-            startPosition: 'URLHash'
-        });
+    var mainCarousel = $(".main-carousel").owlCarousel({
+        items: 2,
+        loop: true,
+        margin: 10,
+        nav: false,
+        dots: false,
+        
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+        // Ваши параметры
     });
-</script>
+
+    // Инициализация нижнего слайдера с миниатюрами
+    // Инициализация нижнего слайдера с миниатюрами
+var thumbnailCarousel = $(".thumbnail-carousel").owlCarousel({
+    items: 2,
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: true,
+    navText: [
+        "<img src='/img/arrow-prev.png'>",
+        "<img src='/img/arrow-next.png'>"
+    ],
+    
+    responsive: {
+        0: {
+            items: 2
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 3
+        }
+    }
+});
+
+    
+    // Связывание двух слайдеров
+    thumbnailCarousel.on('click', '.owl-item', function () {
+        var clickedIndex = $(this).index();
+        mainCarousel.trigger('to.owl.carousel', clickedIndex);
+    });
